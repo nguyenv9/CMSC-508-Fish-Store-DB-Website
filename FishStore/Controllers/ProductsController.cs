ce81623160aabdf77e85aa7e1572c97de19a415e
nguyenv9 <nguyenv9@vcu.edu>
2020-04-29 02:17:53 -0400

    Creating products

---
+++ FishStore/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FishStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FishStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Product/Info
+        public IActionResult Info()
+        {
+            var product = new Product() { Name = "Clownfish" };
+            return View(product);
+        }
+    }
+}
---
+++ FishStore/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FishStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
