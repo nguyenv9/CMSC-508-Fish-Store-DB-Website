00acd53344a046b75fdabeacc46fe00083212fdc
nguyenv9 <nguyenv9@vcu.edu>
2020-04-30 20:53:47 -0400

    adding MySql connection

--- FishStore/Controllers/ProductsController.cs
+++ FishStore/Controllers/ProductsController.cs
@@ -10,10 +10,28 @@ namespace FishStore.Controllers
     public class ProductsController : Controller
     {
         // GET: Product/Info
-        public IActionResult Info()
+        public ViewResult Info()
         {
             var product = new Product() { Name = "Clownfish" };
             return View(product);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        
+        //products
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex, sortBy));
+        }
+
     }
 }
